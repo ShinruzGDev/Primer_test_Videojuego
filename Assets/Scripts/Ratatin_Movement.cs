c81d3a15d3d0125f5e6974e6de62d65608a01dbd
Shinruzzzz <miyukim.art@gmail.com>
2026-04-20 22:24:25 -0500

    Movimiento pero decente
    Ratatín ahora tiene la cámara y una forma de movimiento diferente, solo puede ir hacia atrás o hacia adelante, pero cambia las direcciones girando. Es más que todo para que el sprite tenga sentido :p

--- Assets/Scripts/Ratatin_Movement.cs
+++ Assets/Scripts/Ratatin_Movement.cs
@@ -3,20 +3,22 @@ using UnityEngine;
 public class Ratatin_Movement : MonoBehaviour
 {
     public float speed = 5f;
-    public float rotation = 0f;
+    public float rotation_speed = 200f;
+    Rigidbody2D rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float HorizontalMovement = Input.GetAxis("Horizontal"); 
-        float VerticalMovement = Input.GetAxis("Vertical"); 
+        float HorizontalMovement = Input.GetAxis("Horizontal");
+        Debug.Log(HorizontalMovement);
+        float VerticalMovement = Input.GetAxis("Vertical");
 
-        Vector3 direction = new Vector3(HorizontalMovement, VerticalMovement, 0); 
-        transform.Translate(direction * speed * Time.deltaTime);
+        transform.Rotate(Vector3.forward * -HorizontalMovement * rotation_speed * Time.deltaTime);
+        transform.Translate(Vector3.right * VerticalMovement * speed * Time.deltaTime);
     }
 }
