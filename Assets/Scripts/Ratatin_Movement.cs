54bd056518abdfa5e30ad689032f121691c8a7d0
Shinruzzzz <miyukim.art@gmail.com>
2026-04-20 23:05:16 -0500

    Colliders base
    Se me había olvidado poner los muros jsjsjsj, ya después sigo con los QUESOS que ya ando cansada jsjs. También organicé los recursos en capas.

--- Assets/Scripts/Ratatin_Movement.cs
+++ Assets/Scripts/Ratatin_Movement.cs
@@ -2,20 +2,20 @@ using UnityEngine;
 
 public class Ratatin_Movement : MonoBehaviour
 {
-    public float speed = 5f;
-    public float rotation_speed = 200f;
-    Rigidbody2D rb;
+    public float speed = 5f; //velocidad del jugador
+    public float rotation_speed = 150f; //velocidad de rotación
+    Rigidbody2D rb; //Recolecta info del rigidbody del player
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>(); //Esta línea está aqui por que no quería procesar bien las rotaciones del personaje :p
     }
 
     // Update is called once per frame
     void Update()
     {
         float HorizontalMovement = Input.GetAxis("Horizontal");
-        Debug.Log(HorizontalMovement);
         float VerticalMovement = Input.GetAxis("Vertical");
 
         transform.Rotate(Vector3.forward * -HorizontalMovement * rotation_speed * Time.deltaTime);
