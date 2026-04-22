f68543365af45fc85745096576a9bfca5c0e478c
Shinruzzzz <miyukim.art@gmail.com>
2026-04-21 23:00:34 -0500

    The Real Queso Deluxe!
    El juego está terminado con su primer nivel, abierto a expansiones en un futuro :D

--- Assets/Scripts/Ratatin_Movement.cs
+++ Assets/Scripts/Ratatin_Movement.cs
@@ -8,7 +8,10 @@ public class Ratatin_Movement : MonoBehaviour
     public float rotation_speed = 70f; //velocidad de rotación
     public bool life = true; //la vida de ratatín pq realmente se muere del toque
     public int queso = 0;
+    public int queso2 = 0;
     public TextMeshProUGUI textScore;
+    public TextMeshProUGUI textScoreScpecial;
+    public TextMeshProUGUI notificationText;
     Rigidbody2D rb; //Recolecta info del rigidbody del player
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -32,22 +35,37 @@ public class Ratatin_Movement : MonoBehaviour
         if(other.CompareTag("Trampa"))
         {
             UnityEngine.SceneManagement.SceneManager.LoadScene("Main_Scene");
+            ShowNotification("Te ha matado la trampa!");
         }
 
+        if(other.CompareTag("Agujero") && queso2 == 5)
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("Main_Scene");
+            ShowNotification("Has Ganado Ratatín!");
+        }
 
-        if(other.CompareTag("Queso"))
+        if (other.CompareTag("Agujero") && queso2 != 5)
+        {
+            ShowNotification("Te faltan más Quesos Premium");
+        }
+
+        if (other.CompareTag("Queso"))
         {
             queso = queso + 1;
             UpdateTextScore();
             speed = speed + 0.5f;
             Destroy(other.gameObject);
+            ShowNotification("Yummy!");
         }
 
         if(other.CompareTag("Queso_Dorado"))
         {
             queso = queso + 5;
+            queso2 = queso2 + 1;
             UpdateTextScore();
+            UpdateTextScoreScpecial();
             Destroy(other.gameObject);
+            ShowNotification("Super Yummy!");
         }
 
         if(other.CompareTag("Queso_Azul"))
@@ -56,6 +74,7 @@ public class Ratatin_Movement : MonoBehaviour
             speed = speed - 1;
             UpdateTextScore();
             Destroy(other.gameObject);
+            ShowNotification("Eugh");
         }
 
         if(other.CompareTag("Queso_Blanco"))
@@ -63,10 +82,19 @@ public class Ratatin_Movement : MonoBehaviour
             queso = queso - 5;
             UpdateTextScore();
             Destroy(other.gameObject);
+            ShowNotification("Super Eugh");
         }
     }
     void UpdateTextScore()
     {
         textScore.text = "Queso: " + queso;
+    }
+    void UpdateTextScoreScpecial()
+    {
+        textScoreScpecial.text = "Queso Premium: " + queso2 +"/5";
+    }
+    void ShowNotification(string message)
+    {
+        notificationText.text = message;
     }
 }
